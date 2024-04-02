7af4097ed0769ecff050428d02c52ed92f93add5
DevBauti <devbautisuarez@gmail.com>
2024-04-02 14:12:18 -0300

    '0.0.1'

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -1,6 +1,6 @@
 import random, string
 
-MIN_LENGTH = 6
+MIN_LENGTH = 8
 MAX_LENGTH = 30
 
 class PasswordGenerator:
@@ -15,7 +15,7 @@ class CharacterSet:
     
 class PasswordGeneratorSevice:
     @staticmethod
-    def generate(include_upper=False, include_lower=False, include_digits=False, include_symbols=False):
+    def generate(include_upper=False, include_lower=True, include_digits=True, include_symbols=False):
         characters = ''
         if include_upper:
             characters += string.ascii_uppercase
@@ -36,5 +36,20 @@ class PasswordGeneratorSevice:
         return PasswordGenerator.generate(random.randint(MIN_LENGTH, MAX_LENGTH),cls.generate())
 
     @staticmethod
-    def generate_random_password():
+    def generate_random_length_password_with_all_characters():
         return PasswordGenerator.generate(random.randint(MIN_LENGTH, MAX_LENGTH),CharacterSet.all())
+    
+
+"""
+a = PasswordGeneratorSevice.generate_password()
+b = PasswordGeneratorSevice.generate_random_length_password()
+c = PasswordGeneratorSevice.generate_random_length_password_with_all_characters()
+
+print(f'{a}\n{b}\n{c}')
+
+tsmi6i2s
+grtopz7toq5
+J;Vcg:y-zfE
+
+evidentemente esto no esta terminado
+"""
